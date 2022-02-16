bf2a74267fdfe8c56651f9ae922f93904c50153a
houssemsaada <houssem.saada37@gmail.comgi>
2022-02-16 14:40:00 +0100

    merci de votre attention

--- main.py
+++ main.py
@@ -1,49 +1,48 @@
 import socket
 import json
 import pandas as pd
+from datetime import datetime
 
 UDP_IP = "192.168.162.125"
 UDP_PORT = 5015
 index = 0
 index1 = 0
 
+
 class beacon:
     url: str
     rssi: int
     mac: str
-    def __init__(self, data : dict):
+    ip : str
+    def __init__(self, data: dict):
         self.url = data['URL']
         self.rssi = data['RSSI']
         self.mac = data['Mac']
-    def __repr__(self):
-        return(f' URL={self.url}            rssi= {self.rssi} mac={self.mac}')
+        self.time = datetime.now()
+        self.ip = data['IP']
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+    def __repr__(self):
+        return (f' URL={self.url}            rssi= {self.rssi} mac={self.mac} ip={self.ip}')
+
+
+sock = socket.socket(socket.AF_INET,  # Internet
+                     socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    #print("Type:", type(data))
-    #print("received message: %s" % data)
-    try :
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    # print("Type:", type(data))
+    # print("received message: %s" % data)
+    try:
         datadict = json.loads(data.decode())
         beac = beacon(datadict)
-        print(beac)
-    except :
-        print(data.decode())
-    #print("Type:", type(datadict))
-    #print(datadict['Mac'])
-
-    #pd.DataFrame(datadict)
-    #print(pd.tail(3))
-    #data = json.load(json_file)
-
-    # Print the type of data variable
-
-
-
-
-
+        # print(beac)
+        print(beac.url)
+        print(beac.rssi)
+        print(beac.mac)
+        print(beac.time)
+        print(beac.ip)
+    except:
+        print('exception:', data.decode())
 
 sock.close()
