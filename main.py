cb7412d757958f5eb6732d9f92371f1ab85b10b4
houssemsaada <houssem.saada37@gmail.comgi>
2022-02-17 13:41:38 +0100

    aa

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ UDP_PORT = 5015
 index = 0
 index1 = 0
 
-
 class beacon:
     url: str
     rssi: int
@@ -22,22 +21,40 @@ class beacon:
         self.ip = data['IP']
 
     def __repr__(self):
-        return (f' URL={self.url}            rssi= {self.rssi} mac={self.mac} ip={self.ip} time={self.time}')
+        return (f' URL={self.url}            rssi= {self.rssi} mac={self.mac} ip={self.ip}')
 
 
 sock = socket.socket(socket.AF_INET,  # Internet
                      socket.SOCK_DGRAM)  # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
-while True:
+dict = {}
+i = 0
+
+t1 = datetime.now()
+while (datetime.now()-t1).seconds <= 5:  #run for 5 seconds
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    # print("Type:", type(data))
+    # print("received message: %s" % data)
     try:
         datadict = json.loads(data.decode())
         beac = beacon(datadict)
 
-        print(beac)
+        b_url = beac.url
+        b_rssi = beac.rssi
+        b_mac = beac.mac
+        b_time = beac.time
+        b_ip = beac.ip
+        b_data = []
+        b_data.extend([b_url,b_rssi,b_mac,b_time,b_ip])
 
-    except:
-        print('exception:', data.decode())
+        dict[i] = b_data
+        i = i+1
+
+    except Exception as e:
+        print('exception :', data.decode(), e)
+
+df=pd.DataFrame.from_dict(dict, orient="index", columns= ['URl', 'Rssi', 'Mac', 'Time','ip'])
+print(df[['URl', 'Rssi']])
 
 sock.close()
